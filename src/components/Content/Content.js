66af3375e18609add4590e1cfabc500c2d727663
soromno7 <akolbikcn@mail.ru>
2021-11-08 12:54:02 +0300

    Updated content part

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,13 +1,36 @@
 import "./style.scss";
+import plus from "./img/plus.svg";
+import { useState } from "react";
 
-export function Content({ id, name, imageUrl, price, sizes }) {
+const typesArray = ['тонкое', 'традиционное']
+
+export function Content({ id, name, imageUrl, price, sizes, types }) {
+
+    const [activeType, setActiveType] = useState();
+
     return (
         <>
             <div className="pizzaCardContainer" key={`${name}+${id}`}>
                 <img src={imageUrl} alt={'img num' + {id}}></img>
-                <h5>{name}</h5>
-                <span>Цена: {price} ₽</span>
-                <span>{sizes.map((e) => (<span> {e}см,</span>))}</span>
+                    <h5>{name}</h5>  
+                <div className="pizzaCardBtns">
+                    <div className="typesContainer">
+                        {types.map((identifier) => (
+                        <h6 className={ activeType === typesArray[identifier] ? 'typeItem-active' : 'typeItem'}
+                        key={`${typesArray[identifier]} + ${id}`}
+                        >
+                        {typesArray[identifier]}</h6>))}
+                        </div>
+                    <div className="sizesContainer">{sizes.map((e) => (<h6 className="sizeItem" key={`${e} + ${id}`}> {e} см</h6>))}</div>
+                </div>
+                <div className="pizzaCardBottomContainer">
+                    <h5>от {price} ₽</h5>
+                    <div className="addToCartBtn">
+                        <img src={plus} alt="plus"></img>
+                        <h5>Добавить</h5>
+                    </div>
+                </div>
+
             </div>
         </>
     );
--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import triangle from "./img/triangle.svg";
 import "./style.scss";
 
 export function Sort() {
 
     const sortByList = ['популярности', 'убыванию цены', 'возрастанию цены', 'алфавиту'];
+
+    const [visiblePopup, setVisiblePopup] = useState(false);
+
+    const sortLabel = useRef();
+
+    const clickHandler = (e) => {
+        if(e.path.includes(sortLabel.current)) {
+            setVisiblePopup(!visiblePopup)
+        };
+        if(!e.path.includes(sortLabel.current)) {
+            setVisiblePopup(visiblePopup)
+        };
+    }
+
+    useEffect(() => {
+        document.body.addEventListener("click", clickHandler)
+    }, []);
 
     const [activeCategory, setActiveCategory] = useState(sortByList[0]);
 
@@ -13,9 +30,9 @@ export function Sort() {
             <div className="sort-label">
                 <img src={triangle} alt="triangle" width="10" height="10"></img>
                 <b>Сортировка по: </b>
-                <span>{activeCategory}</span>
+                <span ref={sortLabel}>{activeCategory}</span>
             </div>
-            <div className="sort-popup">
+            <div className={visiblePopup ? "sort-popup" : "sort-popup-hidden"}>
                 <ul>
                     {sortByList.map((name, identifier) => (
                         <li className={activeCategory === name ? 'sortByItem-active' : 'sortByItem'}
